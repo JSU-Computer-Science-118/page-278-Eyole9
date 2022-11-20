75f7886616cb90bfa35b853d40d438ce0a7f4466
Eyole9 <117469913+Eyole9@users.noreply.github.com>
2022-11-19 22:58:54 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,8 +28,8 @@ cout << "Total number of boxes sold: " << totalNumOfBoxesSold << endl;
   cout << " Enter the cost of one box: ";
   cin >> costOfOneBox;
   cout << endl;
-  cout << "Total money made by selling "<< "cookies: $" << totalNumOfBoxesSold * costOfOneBox << endl;
-if (numOfVolunteers != 0)                           {cout << "Average number of "<< "boxes sold by each volunteer: " << totalNumOfBoxesSold / numOfVolunteers << endl;}
+  cout << "Total money made "<<"$" << totalNumOfBoxesSold * costOfOneBox << endl;
+if (numOfVolunteers != 0)                           {cout << "Average number of "<< "boxes sold by each person: " << totalNumOfBoxesSold / numOfVolunteers << endl;}
 else
     {cout << "No input." << endl;           return 0;}
   } 
